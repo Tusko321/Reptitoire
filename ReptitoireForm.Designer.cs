59678623f53d99d5a5f11a541dd769570c12b0de
Kyle Jambretz <kylejambretz@gmail.com>
2024-04-25 01:56:57 -0500

    Added some color

--- ReptitoireForm.Designer.cs
+++ ReptitoireForm.Designer.cs
@@ -574,6 +574,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
